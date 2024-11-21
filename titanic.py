caec8c493d66ebfbd21699d897c4c700af5b5d80
lebondon <filippo.donini@gmail.com>
2024-11-21 14:11:05 +0100

    titanic

--- titanic.py
+++ titanic.py
@@ -4,62 +4,54 @@ import numpy as np
 
 pipe = joblib.load('titanic_pipe.pkl')
 
-#Index(['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
-#  'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
-#   dtype='object'
-
 st.title("Titanic Prediction")
-st.header("inserisci dei possibili valori")
+st.header("Insert possible values")
 
-class_ship=st.selectbox(
+class_ship = st.selectbox(
     "In which class are you on the ship?",
     ("1", "2", "3"),
 )
 
-sex=st.selectbox(
-    "what's your sex?",
+sex = st.selectbox(
+    "What's your sex?",
     ("M", "F"),
 )
 
-age=st.number_input("insert your age",min_value=1,max_value=120)
+age = st.number_input("Insert your age", min_value=1, max_value=120)
 
-sib_flag=st.selectbox(
-    "do you have a spouse or sibling onboard with you?",
+sib_flag = st.selectbox(
+    "Do you have a spouse or sibling onboard with you?",
     ("Yes", "No"),
 )
 
-sib=st.number_input("if yes how many?",min_value=1,max_value=10,key=1)
+sib = st.number_input("If yes, how many?", min_value=1, max_value=10, key=1) if sib_flag == "Yes" else 0
 
-parch_flag=st.selectbox(
-    "do you have parents or childrens onboard with you?",
+parch_flag = st.selectbox(
+    "Do you have parents or children onboard with you?",
     ("Yes", "No"),
 )
 
-parch=st.number_input("if yes how many?",min_value=1,max_value=10,key=2)
+parch = st.number_input("If yes, how many?", min_value=1, max_value=10, key=2) if parch_flag == "Yes" else 0
 
-fare=st.number_input("How much did you pay for a ticket?",min_value=1,max_value=100000)
+fare = st.number_input("How much did you pay for a ticket?", min_value=1, max_value=100000)
 
-embarked=st.selectbox(
+embarked = st.selectbox(
     "Where did you embark from?",
     ("Southampton", "Cherbourg", "Queenstown"),
 )
 
-if sib_flag=='No':
-    sib=0
+embarked_code = {'Southampton': 'S', 'Cherbourg': 'C', 'Queenstown': 'Q'}[embarked]
 
-if parch_flag=='No':
-    parch=0
+values = np.array([class_ship, sex, age, sib, parch, fare, embarked_code]).reshape(1, -1)
 
-if embarked=='Southampton':
-    embarked='S'
-if embarked=='Cherbourg':
-    embarked='C'
-if embarked=='Queenstown':
-    embarked='Q'
-
-values=[class_ship,sex,age,sib,parch,fare,embarked]
-
-print(pipe.predict([class_ship,sex,age,sib,parch,fare,embarked]))
+if st.button("Predict Survival"):
+    
+    prediction = pipe.predict(values)[0]
+    
+    if prediction == 1:
+        st.success("Congratulations! You would survive the Titanic voyage!")
+    else:
+        st.error("Unfortunately, you would not survive the Titanic voyage.")
 
 
 
